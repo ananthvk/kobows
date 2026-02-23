e42cb71ed2e42b7cc73e840a31820d637cbab623
ananthvk <mailstoanantha@gmail.com>
2026-02-24 00:07:51 +0530

    feat: implement writing to file

--- fileupload.go
+++ fileupload.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"html/template"
 	"io"
 	"log/slog"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/gabriel-vasile/mimetype"
 )
@@ -16,7 +19,12 @@ var fileUploadErrorTemplate = template.Must(template.Must(baseTemplate.Clone()).
 func ebookFileUploadHandler(w http.ResponseWriter, r *http.Request) {
 	slog.Info("request details", "method", r.Method, "path", r.URL.Path, "from", r.RemoteAddr)
 	r.Body = http.MaxBytesReader(w, r.Body, maxEbookFileSize)
-	r.ParseMultipartForm(maxEbookFileSize)
+	err := r.ParseMultipartForm(maxEbookFileSize)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fileUploadErrorTemplate.ExecuteTemplate(w, "base", map[string]string{"Error": "Could not read multipart: " + err.Error()})
+		return
+	}
 
 	file, header, err := r.FormFile("ebook")
 	if err != nil {
@@ -42,9 +50,43 @@ func ebookFileUploadHandler(w http.ResponseWriter, r *http.Request) {
 	_, err = file.Seek(0, 0)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		slog.Error("failed to seek file body", "error", err)
 		fileUploadErrorTemplate.ExecuteTemplate(w, "base", map[string]string{"Error": "Internal error"})
 		return
 	}
+
+	// Write to disk
+	destinationFile, err := getDestinationEpubFile(header.Filename)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		slog.Error("failed to create destination file", "error", err)
+		fileUploadErrorTemplate.ExecuteTemplate(w, "base", map[string]string{"Error": "Internal error"})
+	}
+	defer destinationFile.Close()
+
+	if _, err := io.Copy(destinationFile, file); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		slog.Error("failed to copy file", "error", err)
+		fileUploadErrorTemplate.ExecuteTemplate(w, "base", map[string]string{"Error": "Internal error"})
+	}
+	destinationFile.Sync()
+
 	slog.Info("upload success", "filename", header.Filename, "size", header.Size, "mimeType", header.Header, "from", r.RemoteAddr)
 	fileUploadSuccessTemplate.ExecuteTemplate(w, "base", map[string]string{"Filename": header.Filename})
+}
+
+func getDestinationEpubFile(filename string) (*os.File, error) {
+	filename = filepath.Clean(filename)
+	filename = filepath.Base(filename)
+
+	if !filepath.IsLocal(filename) {
+		return nil, errors.New("invalid filename")
+	}
+
+	if filepath.Ext(filename) != ".epub" {
+		filename += ".epub"
+	}
+
+	destination := filepath.Join(uploadBaseDirectory, filename)
+	return os.Create(destination)
 }
--- main.go
+++ main.go
@@ -6,21 +6,31 @@ import (
 	"html/template"
 	"log/slog"
 	"net/http"
+	"os"
 )
 
 var baseTemplate = template.Must(template.ParseFiles("templates/base.gohtml"))
+var uploadBaseDirectory = "/mnt/onboard/uploads"
 
 func main() {
 	portPtr := flag.Uint("port", 8000, "specify the port on which to listen")
 	hostPtr := flag.String("host", "0.0.0.0", "specify the bind address")
+	uploadsPath := flag.String("upload-path", "/mnt/onboard/uploads/", "specify the upload directory")
 	flag.Parse()
+
 	address := fmt.Sprintf("%s:%d", *hostPtr, *portPtr)
+	uploadBaseDirectory = *uploadsPath
+
+	if err := os.MkdirAll(uploadBaseDirectory, 0o755); err != nil {
+		slog.Error("failed to create upload directory", "error", err, "path", uploadBaseDirectory)
+		return
+	}
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /system-info", sysinfoHandler)
 	mux.HandleFunc("POST /upload", ebookFileUploadHandler)
 	mux.HandleFunc("GET /", indexHandler)
 
-	slog.Info("listening on", "address", address)
+	slog.Info("listening on", "address", address, "upload-path", *uploadsPath)
 	http.ListenAndServe(address, mux)
 }
